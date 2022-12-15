f9623797293985cd385275d4435333d2a91ab50a
Peter Kőrösi-Szabó <koszpe@gmail.com>
2022-12-15 10:36:57 +0100

    add arguments

--- main_multi_mnist.py
+++ main_multi_mnist.py
@@ -7,88 +7,103 @@ from net.lenet import MultiLeNetR, MultiLeNetO
 from optimizers.pcgrad import PCGrad
 from utils import create_logger
 
-# ------------------ CHANGE THE CONFIGURATION -------------
-PATH = './dataset'
-LR = 0.0005
-BATCH_SIZE = 256
-NUM_EPOCHS = 100
-TASKS = ['R', 'L']
-DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
-# ---------------------------------------------------------
+import argparse
 
 
-accuracy = lambda logits, gt: ((logits.argmax(dim=-1) == gt).float()).mean()
-to_dev = lambda inp, dev: [x.to(dev) for x in inp]
-logger = create_logger('Main')
+def main(args):
+    DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+    # ---------------------------------------------------------
 
-global_transformer = transforms.Compose(
-    [transforms.ToTensor(),
-     transforms.Normalize((0.1307, ), (0.3081, ))])
+    accuracy = lambda logits, gt: ((logits.argmax(dim=-1) == gt).float()).mean()
+    to_dev = lambda inp, dev: [x.to(dev) for x in inp]
+    logger = create_logger('Main')
 
-train_dst = MultiMNIST(PATH,
-                       train=True,
-                       download=True,
-                       transform=global_transformer,
-                       multi=True)
-train_loader = torch.utils.data.DataLoader(train_dst,
-                                           batch_size=BATCH_SIZE,
-                                           shuffle=True,
-                                           num_workers=4)
+    global_transformer = transforms.Compose(
+        [transforms.ToTensor(),
+         transforms.Normalize((0.1307,), (0.3081,))])
 
-val_dst = MultiMNIST(PATH,
-                     train=False,
-                     download=True,
-                     transform=global_transformer,
-                     multi=True)
-val_loader = torch.utils.data.DataLoader(val_dst,
-                                         batch_size=100,
-                                         shuffle=True,
-                                         num_workers=1)
-nets = {
-    'rep': MultiLeNetR().to(DEVICE),
-    'L': MultiLeNetO().to(DEVICE),
-    'R': MultiLeNetO().to(DEVICE)
-}
-param = [p for v in nets.values() for p in list(v.parameters())]
-optimizer = torch.optim.Adam(param, lr=LR)
-optimizer = PCGrad(optimizer)
+    train_dst = MultiMNIST(args.data_path,
+                           train=True,
+                           download=True,
+                           transform=global_transformer,
+                           multi=True)
+    train_loader = torch.utils.data.DataLoader(train_dst,
+                                               batch_size=args.batch_size,
+                                               shuffle=True,
+                                               num_workers=4)
 
-for ep in range(NUM_EPOCHS):
-    for net in nets.values():
-        net.train()
-    for batch in train_loader:
-        mask = None
-        optimizer.zero_grad()
-        img, label_l, label_r = to_dev(batch, DEVICE)
-        rep, mask = nets['rep'](img, mask)
-        out_l, mask_l = nets['L'](rep, None)
-        out_r, mask_r = nets['R'](rep, None)
+    val_dst = MultiMNIST(args.data_path,
+                         train=False,
+                         download=True,
+                         transform=global_transformer,
+                         multi=True)
+    val_loader = torch.utils.data.DataLoader(val_dst,
+                                             batch_size=100,
+                                             shuffle=True,
+                                             num_workers=1)
+    nets = {
+        'rep': MultiLeNetR().to(DEVICE),
+        'L': MultiLeNetO().to(DEVICE),
+        'R': MultiLeNetO().to(DEVICE)
+    }
+    param = [p for v in nets.values() for p in list(v.parameters())]
+    optimizer = getattr(torch.optim, args.inner_optimizer)(param, lr=args.lr)
+    optimizer = PCGrad(optimizer)
 
-        losses = [F.nll_loss(out_l, label_l), F.nll_loss(out_r, label_r)]
-        optimizer.multi_loss_backward(losses)
-        # sum(losses).backward()
-        optimizer.step()
+    for ep in range(args.num_epoch):
+        for net in nets.values():
+            net.train()
+        for batch in train_loader:
+            mask = None
+            optimizer.zero_grad()
+            img, label_l, label_r = to_dev(batch, DEVICE)
+            rep, mask = nets['rep'](img, mask)
+            out_l, mask_l = nets['L'](rep, None)
+            out_r, mask_r = nets['R'](rep, None)
 
-    losses, acc = [], []
-    for net in nets.values():
-        net.eval()
-    for batch in val_loader:
-        img, label_l, label_r = to_dev(batch, DEVICE)
-        mask = None
-        rep, mask = nets['rep'](img, mask)
-        out_l, mask_l = nets['L'](rep, None)
-        out_r, mask_r = nets['R'](rep, None)
+            losses = [F.nll_loss(out_l, label_l), F.nll_loss(out_r, label_r)]
+            optimizer.multi_loss_backward(losses)
+            # sum(losses).backward()
+            optimizer.step()
 
-        losses.append([
-            F.nll_loss(out_l, label_l).item(),
-            F.nll_loss(out_r, label_r).item()
-        ])
-        acc.append(
-            [accuracy(out_l, label_l).item(),
-             accuracy(out_r, label_r).item()])
-    losses, acc = np.array(losses), np.array(acc)
-    logger.info('epoches {}/{}: loss (left, right) = {:5.4f}, {:5.4f}'.format(
-        ep, NUM_EPOCHS, losses[:,0].mean(), losses[:,1].mean()))
-    logger.info(
-        'epoches {}/{}: accuracy (left, right) = {:5.3f}, {:5.3f}'.format(
-            ep, NUM_EPOCHS, acc[:,0].mean(), acc[:,1].mean()))
+        losses, acc = [], []
+        for net in nets.values():
+            net.eval()
+        for batch in val_loader:
+            img, label_l, label_r = to_dev(batch, DEVICE)
+            mask = None
+            rep, mask = nets['rep'](img, mask)
+            out_l, mask_l = nets['L'](rep, None)
+            out_r, mask_r = nets['R'](rep, None)
+
+            losses.append([
+                F.nll_loss(out_l, label_l).item(),
+                F.nll_loss(out_r, label_r).item()
+            ])
+            acc.append(
+                [accuracy(out_l, label_l).item(),
+                 accuracy(out_r, label_r).item()])
+        losses, acc = np.array(losses), np.array(acc)
+        logger.info('epoches {}/{}: loss (left, right) = {:5.4f}, {:5.4f}'.format(
+            ep, args.num_epoch, losses[:, 0].mean(), losses[:, 1].mean()))
+        logger.info(
+            'epoches {}/{}: accuracy (left, right) = {:5.3f}, {:5.3f}'.format(
+                ep, args.num_epoch, acc[:, 0].mean(), acc[:, 1].mean()))
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='Inference script for trained models.')
+
+    parser.add_argument("--data-path", type=str, default="/storage/Pytorch-PCGrad/data")
+    parser.add_argument("--output_dir", type=str, default="/storage/Pytorch-PCGrad/finetune/lines")
+
+    parser.add_argument("--inner_optimizer", type=str, default="Adam")
+    parser.add_argument("--lr", type=float, default=0.0005)
+    parser.add_argument("--batch-size", type=int, default=256)
+    parser.add_argument("--num-epoch", type=int, default=100)
+
+    parser.add_argument("--line_num", type=int, default=100000)
+
+    args = parser.parse_args()
+
+    main(args)
